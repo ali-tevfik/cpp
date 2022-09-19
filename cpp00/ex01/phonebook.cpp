61c78d2e8691dd9873dade848c44fb61c4fe775e
ali-tevfik <tevfikdoner@hotmail.com>
2022-09-19 15:37:19 +0200

    update std::cin

--- cpp00/ex01/phonebook.cpp
+++ cpp00/ex01/phonebook.cpp
@@ -12,18 +12,7 @@ PHONEBOOK::~PHONEBOOK()
 }
 
 //add new user. First get contact information and then check how many contact there have. if there are 8 contact replace last one and add.
-// void PHONEBOOK::add_new_user()
-// {
-//     CONTACT contact;
-//     contact = contact.get_new_contact_info();
-//     if (this->size == 8)
-//       this->contact[7] = contact;
-//     else
-//     {
-//       this->contact[this->size] = contact;
-//       this->size++;
-//     }
-// }
+
 
 void PHONEBOOK::add_new_user()
 {
@@ -87,39 +76,44 @@ void all_info(CONTACT& contact)
 //search
 void PHONEBOOK::show_user(PHONEBOOK& phonebok)
 {
-  std::string s_index;
-  int i_index;
-  std::cout << "---------------------------------------------" << std::endl;
-  std::cout << "|";
-  print_user_info("index");
-  print_user_info("first name");
-  print_user_info("last name");
-  print_user_info("nickname");
-  std::cout << std::endl;
-  std::cout << "---------------------------------------------" << std::endl;
-
-  for (int i = 0; i < this->size; i++)
-  {
-    std::cout << "|";
-    print_user_info(std::to_string(i + 1));
-    print_user_info(phonebok.contact[i].first_name);
-    print_user_info(phonebok.contact[i].last_name);
-    print_user_info(phonebok.contact[i].nick_name);
-  std::cout << std::endl << "---------------------------------------------" << std::endl;
-  }
-  std::cout << std::endl << "Please select index number!"<<std::endl;
-  std::cin >> s_index;
-  try
-  {
-    i_index = std::stoi(s_index);
-  }
-  catch(const std::exception& e)
-  {
-    i_index = -1;
-  }
-  
-  if (i_index < this->size + 1 && i_index != -1)
-    all_info(phonebok.contact[i_index - 1]);
+  if (this->size == 0)
+    std::cout << "Your phonebook is emty!!" << std::endl;
   else
-    std::cout << "wrong index!!!"<<std::endl;
+  {
+    std::string s_index;
+    int i_index;
+    std::cout << "---------------------------------------------" << std::endl;
+    std::cout << "|";
+    print_user_info("index");
+    print_user_info("first name");
+    print_user_info("last name");
+    print_user_info("nickname");
+    std::cout << std::endl;
+    std::cout << "---------------------------------------------" << std::endl;
+
+    for (int i = 0; i < this->size; i++)
+    {
+      std::cout << "|";
+      print_user_info(std::to_string(i + 1));
+      print_user_info(phonebok.contact[i].first_name);
+      print_user_info(phonebok.contact[i].last_name);
+      print_user_info(phonebok.contact[i].nick_name);
+    std::cout << std::endl << "---------------------------------------------" << std::endl;
+    }
+    std::cout << std::endl << "Please select index number!"<<std::endl;
+    std::getline(std::cin, s_index);
+    try
+    {
+      i_index = std::stoi(s_index);
+    }
+    catch(const std::exception& e)
+    {
+      i_index = -1;
+    }
+    
+    if (i_index < this->size + 1 && i_index != -1)
+      all_info(phonebok.contact[i_index - 1]);
+    else
+      std::cout << "wrong index!!!"<<std::endl;
+  }
 }
