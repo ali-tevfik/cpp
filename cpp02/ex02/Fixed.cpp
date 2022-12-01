1a7f118dc0b0335df4141c016abf1c7bc93947df
ali-tevfik <tevfikdoner@hotmail.com>
2022-12-01 15:45:58 +0100

    update cpp02

--- cpp02/ex02/Fixed.cpp
+++ cpp02/ex02/Fixed.cpp
@@ -3,45 +3,37 @@
 //constructor
 Fixed::Fixed(/* args */)
 {
-	std::cout << "Default Constructor called" << std::endl;
 	_value = 0;
 }
 //constructor
 Fixed::Fixed(int value)
 {
 	_value = value << _fractionalBits;
-	std::cout << "int Constructor called" << std::endl;
 }
 //constructor
 Fixed::Fixed(const float value)
 {
 	_value = (int)std::roundf( value * (1 << _fractionalBits));
-	printf("a  =   %d \n",_value);
 	
-	std::cout << "float Constructor called" << std::endl;
 }
 
 ///copy
 Fixed::Fixed(const Fixed &fixed)
 {
-	std::cout << "Copy Constructor called" << std::endl;
 	*this = fixed;
 }
 
 //destructor
 Fixed::~Fixed()
 {
-	std::cout << "Destructor called" << std::endl;
 }
 
 
 //setter-getter
 int Fixed::getRawBits() const {
-	 std::cout <<"getRawBits member function called" << std::endl;
 	return _value;
 }
 void Fixed::setRawBits(int const raw) {
-	 std::cout <<"setRawBits member function called" << std::endl;
 	_value = raw;
 }
 
@@ -56,7 +48,6 @@ std::ostream &operator<<( std::ostream &o, const Fixed &fixed){
 
 Fixed& Fixed::operator=(const Fixed &fixed)
 {
-	std::cout << "Copy assignment operator called" << std::endl;
 	_value = fixed.getRawBits();
 	return *this;
 }
